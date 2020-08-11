1adcb7feada290a6dc2e01a986a519c5604e2bb2
MAtterholt <matterholt84@gmail.com>
2020-08-10 23:05:51 -0400

    example one closure

--- Closure_Examples/codeExample_1.js
+++ Closure_Examples/codeExample_1.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 function lookUpStudent(studentID) {
     let students = [
@@ -7,9 +7,19 @@ function lookUpStudent(studentID) {
         { id: 112, name: "Frank" },
         { id: 6, name: "Sarah" }
     ]
-    return function greetStudent() {
+    return function greetStudent(greeting) {
         let student = students.find(
             student => student.id === studentID
-        )
-    }
+        );
+        return `${greeting} , ${student.name}!`
+    };
+
 }
+
+let chosenStudent = [
+    lookUpStudent(14),
+    lookUpStudent(112),
+]
+console.log(chosenStudent[0].name)
+console.log(chosenStudent[0]('hey'))
+console.log(chosenStudent[1]('hi'))
